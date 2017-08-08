87baaf69ae86b488b11c48aeb76df7d358acca81
Jli0423 <justin.li0423@gmail.com>
2017-08-08 00:16:33 -0400

    setup rest of the partials

--- app.js
+++ app.js
@@ -14,6 +14,26 @@ app.get('/about-us', (req, res)=>{
   res.render('about');
 });
 
+app.get('/services', (req, res)=>{
+  res.render('services');
+});
+
+app.get('/schedule', (req, res)=>{
+  res.render('schedule');
+});
+
+app.get('/registration', (req, res)=>{
+  res.render('registration');
+});
+
+app.get('/testimonials', (req, res)=>{
+  res.render('testimonials');
+});
+
+app.get('/contact-us', (req, res)=>{
+  res.render('contact');
+});
+
 app.listen(3000, ()=>{
   console.log('Server is up on port 3000');
 });
