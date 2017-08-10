361fbb72e812b5652a9680aee35eba3f7b63adfb
Jli0423 <justin.li0423@gmail.com>
2017-08-10 14:47:20 -0400

    added animation to car

--- views/js/animate.js
+++ views/js/animate.js
@@ -3,3 +3,14 @@ $(document).ready(function(){
   'use strict';
   new WOW().init();
 });
+
+// Car move right animation
+$("div.lambo").hover(function(){
+  $(this).stop(true,false).animate({
+    marginLeft: "300px"
+  });
+}, function(){
+  $(this).stop(true,false).animate({
+    marginLeft: "0px"
+  });
+});
