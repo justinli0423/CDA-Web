05d963b4367460b1146d914d37def366e49caafa
Jli0423 <justin.li0423@gmail.com>
2017-08-10 18:29:16 -0400

    added jquery ui

--- views/js/animate.js
+++ views/js/animate.js
@@ -7,18 +7,54 @@ $(document).ready(function(){
 // Car move right animation
 $("div.gray").hover(function(){
   $("div.lambo").stop(true, false).animate({
-    marginLeft: "85%"
+    marginLeft: "30%"
   });
 
-  $("#register").stop(true, false).animate({
-   'text-indent': "30%"
+  $("#register").stop(true, false).delay(100).animate({
+   'text-indent': "0%",
+   'left': "50%"
+  });
+
+  $(".register-box").delay(300).animate({
+    'borderTopColor': "#e71d26",
+    'borderLeftColor': "#e71d26",
+    'borderRightColor': "#e71d26",
+    'borderBottomColor': "#e71d26"
   });
 }, function(){
-  $("div.lambo").stop(true, false).animate({
-    marginLeft: "0px"
-  });
+  $(".register-box").animate({
+    'background-color': "transparent",
+    'color': "#e71d26"
+  }, {
+    complete: function(){
+    $(".register-box").animate({
+      'borderTopColor': "transparent",
+      'borderLeftColor': "transparent",
+      'borderRightColor': "transparent",
+      'borderBottomColor': "transparent"
+    },{
+      complete: function(){
+        $("#register").stop(true, false).animate({
+          'text-indent': "-200px"
+        },{
+          complete: function(){
+            $("div.lambo").stop(true, false).animate({
+              marginLeft: "0px"
+            });
+          }});
+      }});
+  }});
 
-  $("#register").stop(true, false).animate({
-    'text-indent': "-200px"
+});
+
+$(".register-box").hover(function(){
+  $(this).animate({
+    'background-color': "#e71d26",
+    'color': "white"
+  });
+}, function(){
+  $(this).animate({
+    'background-color': "transparent",
+    'color': "#e71d26"
   });
 });
