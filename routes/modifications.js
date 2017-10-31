8e1717c4ad9c792946cfbbcdd866494d5211223b
jli0423 <justin.li0423@gmail.com>
2017-10-30 23:11:41 -0400

    Db update test 2

--- routes/modifications.js
+++ routes/modifications.js
@@ -1,68 +1,39 @@
 module.exports = function(app, isLoggedIn, MongoClient, urlencodedParser){
-  // var {Home} = require('./models/home');
+  // acquire schema
+  var {Home} = require('../models/home');
 
   app.get('/auth-home', isLoggedIn, (req, res) => {
-    MongoClient.connect("mongodb://cda_web:Justin_199898li@ds127982.mlab.com:27982/heroku_d48w58zm", (err, db) =>{
-      db.collection("home", (err, collection) => {
-        collection.find().toArray((err, result) => {
-          if(err){
-            return err;
-          }
-          res.render('auth/auth-home', {
-            title1: result[0].title1,
-            text1: result[0].text1,
-            subtitle1: result[0].subtitle1,
-            text2: result[0].text2,
-            subtitle2: result[0].subtitle2
-          });
-        });
+    var query = {title1: /^/};
+    Home.findOne(query, (err, doc) => {
+      if(err){
+        return err;
+      }
+      res.render('auth/auth-home', {
+        title1: doc.title1,
+        text1: doc.text1,
+        subtitle1: doc.subtitle1,
+        subtitle2: doc.subtitle2,
+        text2: doc.text2
       });
     });
   });
 
-  // Send newly changed data background-color
   app.post('/modify', urlencodedParser, (req, res) => {
-    MongoClient.connect("mongodb://cda_web:Justin_199898li@ds127982.mlab.com:27982/heroku_d48w58zm", (err, db) => {
-      db.collection("home", (err, collection) => {
-        var querytitle1 = {title1: /^/};
-        var title1 = {$set: {title1: req.body.title1}};
-        var querytext1 = {text1: /^/};
-        var text1 = {$set: {text1: req.body.text1}};
-        var querysubtitle1 = {subtitle1: /^/};
-        var subtitle1 = {$set: {subtitle1: req.body.subtitle1}};
-        var querytext2 = {text2: /^/};
-        var text2 = {$set: {text2: req.body.text2}};
-        var querysubtitle2 = {subtitle2: /^/};
-        var subtitle2 = {$set: {subtitle2: req.body.subtitle2}};
-        collection.updateOne(querytitle1, title1, (err, res) => {
-          if(err){
-            return err;
-          }
-        });
-        collection.updateOne(querytext1, text1, (err, res) => {
-          if(err){
-            return err;
-          }
-        });
-        collection.updateOne(querysubtitle1, subtitle1, (err, res) => {
-          if(err){
-            return err;
-          }
-        });
-        collection.updateOne(querytext2, text2, (err, res) => {
-          if(err){
-            return err;
-          }
-        });
-        collection.updateOne(querysubtitle2, subtitle2, (err, res) => {
-          if(err){
-            return err;
-          }
-        });
-      });
-    });
     res.redirect('/profile');
-
+    var query = {title1: /^/};
+    // upsert creates if query isn't found
+    Home.findOneAndUpdate(query, {
+      title1: req.body.title1,
+      text1: req.body.text1,
+      subtitle1: req.body.subtitle1,
+      subtitle2: req.body.subtitle2,
+      text2: req.body.text2
+    }, {upsert:true}, (err, doc) => {
+      if(err){
+        return err;
+      }
+      console.log("success");
+    });
   });
 
   app.get('/auth-aboutus', isLoggedIn, (req, res) => {
